ccc6401d4ab94579dded04c369cb099ee1b44325
FernandoSullca <sullcafernando18@gmail.com>
2019-11-01 10:35:45 -0300

    Arregaldoo dimensiones.....

--- src/AlgoritmoCalidadSimplePago/FrameFiabilidad.java
+++ src/AlgoritmoCalidadSimplePago/FrameFiabilidad.java
@@ -72,7 +72,7 @@ public class FrameFiabilidad extends JFrame {
 		txtpnDescripcinEsLa.setText("\u25CF       Subcaracter\u00EDstica: Tolerancia a fallos\r\nDescripci\u00F3n:\r\nEs la capacidad del producto software de mantener la integridad de los datos cuando se producen fallas del sistema.\r\n \r\nCaracter\u00EDsticas a medir\r\n\u2022 Cuando sucede un error se protegen los datos procesados.\r\n\u2022 Se realiza un log de actividades que el sistema estaba haciendo.");
 		txtpnDescripcinEsLa.setEditable(false);
 		txtpnDescripcinEsLa.setBackground(new Color(255, 255, 224));
-		txtpnDescripcinEsLa.setBounds(13, 31, 298, 111);
+		txtpnDescripcinEsLa.setBounds(13, 31, 298, 151);
 		contentPane.add(txtpnDescripcinEsLa);
 		
 		JSeparator separator_1 = new JSeparator();
@@ -83,7 +83,7 @@ public class FrameFiabilidad extends JFrame {
 		txtpnDescripcin.setText("\u25CF        Subcaracter\u00EDstica: Capacidad de recuperaci\u00F3n de errores\r\nDescripci\u00F3n:\r\nEs la capacidad del sistema de reanudar sus actividades cuando se producen errores cr\u00EDticos.\r\n \r\nCaracter\u00EDsticas a medir\r\n\u2022 El sistema reanuda las actividades si se produce una falla cr\u00EDtica.\r\n\u2022 Reanuda sus actividades y vuelve al estado en que estaba.");
 		txtpnDescripcin.setEditable(false);
 		txtpnDescripcin.setBackground(new Color(255, 255, 224));
-		txtpnDescripcin.setBounds(13, 210, 298, 136);
+		txtpnDescripcin.setBounds(13, 189, 298, 161);
 		contentPane.add(txtpnDescripcin);
 		
 		/******ComboBoxs******/
@@ -117,7 +117,7 @@ public class FrameFiabilidad extends JFrame {
 		contentPane.add(ToleranciaBox);
 	
 		JLabel label_1 = new JLabel("Evaluacion");
-		label_1.setBounds(328, 164, 237, 14);
+		label_1.setBounds(328, 189, 237, 14);
 		contentPane.add(label_1);
 		
 		JComboBox CapacidadBox = new JComboBox();
@@ -141,7 +141,7 @@ public class FrameFiabilidad extends JFrame {
 		});
 		CapacidadBox.setModel(new DefaultComboBoxModel(new String[] {"No cumple con ninguna caracter\u00EDstica", "Cumple con 1 caracter\u00EDstica", "Cumple con 2 caracter\u00EDsticas"}));
 		CapacidadBox.setToolTipText("Evaluaci\u00F3n\r\nNo cumple con ninguna caracter\u00EDstica. \u2192 0 puntos\r\nCumple con 1 caracter\u00EDstica. \u2192 1 punto\r\nCumple con 2 caracter\u00EDsticas. \u2192 2 puntos\r\n");
-		CapacidadBox.setBounds(328, 189, 237, 20);
+		CapacidadBox.setBounds(328, 210, 237, 20);
 		contentPane.add(CapacidadBox);
 		
 		/*****Boton Siguiente****/
